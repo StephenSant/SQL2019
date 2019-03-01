2be40484fefbe688bcf79ce4ac1dc6062d1ac597
stephen <stephen.sant123@gmail.com>
2019-03-01 12:06:40 +1100

    Can Create Account

---
+++ Assets/Scripts/CreateAccount.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateAccount : MonoBehaviour
+{
+    #region Variables
+    public InputField
+        username,
+        password,
+        email,
+        confEmail;
+    public GameObject textField;
+    public Text text;
+    #endregion
+
+    public void CreateUser()
+    {
+        if (email.text == confEmail.text)
+        {
+            StartCoroutine(CreateUser(username.text, password.text, email.text));
+        }
+        else
+        {
+            Debug.Log("Your emails don't match.");
+            text.text = "Your emails don't match.";
+        }
+    }
+
+    IEnumerator CreateUser(string _username, string _password, string _email)
+    {
+        string createUserURL = "http://localhost/squealsystem/InsertUser.php";
+
+        WWWForm insertUserForm = new WWWForm();
+        insertUserForm.AddField("usernamePost", _username);
+        insertUserForm.AddField("passwordPost", _password);
+        insertUserForm.AddField("emailPost", _email);
+
+        WWW www = new WWW(createUserURL, insertUserForm);
+
+        yield return www;
+
+        Debug.Log(www.text);
+
+        if(www.text == "User Already ExistsEmail Already Exists")
+        {
+            text.text = "User Already Exists";
+        }
+        else
+        {
+            text.text = www.text;
+        }
+        textField.SetActive(www.text != "");
+    }
+
+
+}
