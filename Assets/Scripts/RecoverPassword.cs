9650f2e47fc08adfa31a21099abcbd88567370ee
stephen <stephen.sant123@gmail.com>
2019-03-15 12:01:54 +1100

    yeet

--- Assets/Scripts/RecoverPassword.cs
+++ Assets/Scripts/RecoverPassword.cs
@@ -15,11 +15,20 @@ public class RecoverPassword : MonoBehaviour
     public string code;
     public string username;
     public InputField email, codeCheck;
-    public void SendEmail()
+    public InputField enterPassword, reEnterPassword;
+
+    public GameObject panel1, panel2, panel3;
+
+    public void FindUser()
+    {
+        StartCoroutine(FindUsername(email.text));
+    }
+
+    public void SendEmail(string email, string username)
     {
         code = RandomString(6);
         MailMessage mail = new MailMessage();
-        mail.To.Add(email.text);
+        mail.To.Add(email);
         mail.From = new MailAddress("stephen.sant123@gmail.com", "E-mail Bot 3000");
         mail.Subject = "SQueaL Password Reset";
         mail.Body = "Hello " + username + ",\nTo reset your password enter this code into your game: " + code;
@@ -32,7 +41,11 @@ public class RecoverPassword : MonoBehaviour
 
         smtpServer.Send(mail);
         Debug.Log("Email sent");
+        
+        OpenPanel(2);
     }
+
+    
 
     public static string RandomString(int length)
     {
@@ -45,10 +58,77 @@ public class RecoverPassword : MonoBehaviour
         if (codeCheck.text == code)
         {
             Debug.Log("Success");
+            OpenPanel(3);
         }
         else
         {
             Debug.Log("Failed");
+        }
+    }
+
+    public void ChangePasswordFunction()
+    {
+        if (enterPassword.text == reEnterPassword.text)
+        {
+            StartCoroutine(ChangePassword(enterPassword.text,email.text));
+        }
+        else
+        {
+            Debug.Log("Passwords must be the same!");
+        }
+    }
+
+    IEnumerator FindUsername(string _email)
+    {
+        string FindUserURL = "http://localhost/squealsystem/CheckUser.php";
+        WWWForm form = new WWWForm();
+        form.AddField("emailPost", _email);
+        WWW www = new WWW(FindUserURL, form);
+        yield return www;
+        Debug.Log(www.text);
+        username = www.text;
+
+        SendEmail(_email, username);
+    }
+
+    IEnumerator ChangePassword(string _newPassword,string _email)
+    {
+        string ChangePasswordURL = "http://localhost/squealsystem/UpdatePassword.php";
+        WWWForm form = new WWWForm();
+        form.AddField("passwordPost", _newPassword);
+        form.AddField("emailPost", _email);
+        WWW www = new WWW(ChangePasswordURL, form);
+        yield return www;
+        Debug.Log(www.text);
+        if (www.text == "Password Changed")
+        {
+            Debug.Log("Good Job!");
+        }
+        Debug.Log(www.text);
+    }
+
+    public void OpenPanel(int panel)
+    {
+        switch (panel)
+        {
+            case 1:
+                panel1.SetActive(true);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                break;
+            case 2:
+                panel1.SetActive(false);
+                panel2.SetActive(true);
+                panel3.SetActive(false);
+                break;
+            case 3:
+                panel1.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(true);
+                break;
+            default:
+                Debug.LogError("Can only open panel 1, 2 or 3.");
+                break;
         }
     }
 }
